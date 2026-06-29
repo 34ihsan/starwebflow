78a917e8ab1a8b2e16f2443e090bd87b681e3881
Ihsan Inan <ihsaninan34@gmail.com>
2026-06-30 01:32:49 +0200

    fix(pm2): change exec_mode to cluster in ecosystem.config.js to resolve EADDRINUSE conflicts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "node_modules/next/dist/bin/next",
       args: "start",
       instances: "max",
-      exec_mode: "fork",
+      exec_mode: "cluster",
       watch: false,
       max_memory_restart: "1G",
       env: {
